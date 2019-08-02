185e69963c522feef6f43d232de1f686e079928f
TonyHe <he@holptech.com>
2019-08-02 13:46:35 +0800

    Change name to Eugrade

--- pro/pro_header.php
+++ pro/pro_header.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Pokers</title>
+    <title>Eugrade Inside</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="Shortcut Icon" href="../statics/img/pokers_icon.ico" type="image/x-icon">
